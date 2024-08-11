d0762dcb3ebfa64bca5ef80eb9d39db356bb11c6
Paul Adams <nihaopaul@gmail.com>
2024-08-11 16:29:14 +0800

    cleanup

--- default.nix
+++ default.nix
@@ -1,3 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.callPackage ./derivation.nix {}
+with import <nixpkgs> {};
+let
+  in stdenv.mkDerivation rec {
+    name = "env"; 
+    env = buildEnv {
+      name = name;
+      paths = buildInputs;
+    };
+    buildInputs = [
+      nodejs-18_x
+    ];
+}
